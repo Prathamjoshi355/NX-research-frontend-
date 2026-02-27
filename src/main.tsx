29f548cfa9993a172e4219b43a81f27134435050
Prathamjoshi355 <prathamjoshi355@gmail.com>
2026-02-28 01:32:08 +0530

    Remove react-helmet-async wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
+// import { HelmetProvider } from 'react-helmet-async';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    {/* HelmetProvider removed because dependency conflict with React 19 */}
+    <App />
   </StrictMode>,
 );
